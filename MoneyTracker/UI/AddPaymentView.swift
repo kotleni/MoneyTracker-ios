54f03e6599926615162760a57a20a3d749aee7bb
kotleni <yavarenikya@gmail.com>
2022-07-16 11:17:29 +0300

    Updated ui for adding payment

--- MoneyTracker/UI/AddPaymentView.swift
+++ MoneyTracker/UI/AddPaymentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AddPaymentView: View {
     @State private var priceText: String = ""
     @State private var aboutText: String = ""
-    @State private var spendingBool: Bool = false
+    @State private var spendingBool: Bool = true
     @State private var selectedTag: Tag = Tag.other
     
     @Binding var isSheetShow: Bool
@@ -19,83 +19,90 @@ struct AddPaymentView: View {
     
     var body: some View {
         NavigationView {
-            Form {
-                Section {
-                    HStack {
-                        Text("field_price".localized)
-                        Spacer()
-                        TextField("hint_necessarily".localized, text: $priceText)
-                            .keyboardType(.decimalPad)
-                            .multilineTextAlignment(.trailing)
-                            .frame(width: UIScreen.main.bounds.width / 3)
-                    }
-                    HStack {
-                        Text("field_about".localized)
-                        Spacer()
-                        TextField("hint_necessarily".localized, text: $aboutText)
-                            .multilineTextAlignment(.trailing)
-                            .frame(width: UIScreen.main.bounds.width / 3)
-                    }
-                    HStack {
-                        Text("field_expenses".localized)
-                        Toggle("", isOn: $spendingBool)
-                    }
-                    if spendingBool {
-                        HStack {
-                            Text("Тэг".localized)
-                            Spacer()
-                            Picker("", selection: $selectedTag) {
-                                ForEach(Tag.allCases, id: \.self) { tag in
-                                    Text(tag.rawValue.localized)
-                                }
-                            }
-                            .pickerStyle(.menu)
-                        }
-                        .animation(.default)
-                        .transition(.slide)
-                    }
-                } footer: {
-                    Text("hint_payment".localized)
+            VStack {
+                Picker("", selection: $spendingBool) {
+                    Text("segment_expenses".localized)
+                        .tag(true)
+                    Text("segment_income".localized)
+                        .tag(false)
                 }
-            }
-            .navigationBarTitle("label_newpayment".localized, displayMode: .inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button {
-                        isSheetShow = false
-                    } label: {
-                        Text("btn_cancel".localized)
-                    }
-                }
+                .pickerStyle(.segmented)
+                .padding()
                 
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        if !priceText.isEmpty && !aboutText.isEmpty {
-                            let priceStr = priceText.replacingOccurrences(of: ",", with: ".")
-                            guard let fl = Float(spendingBool ? "-\(priceStr)" : priceStr) else { return }
-                            let about = aboutText
-                            let tag = selectedTag
-                            
-                            DispatchQueue.global(qos: .userInitiated).async {
-                                CoreDataManager.shared.addPayment(price: fl, about: about, tag: tag)
-                                
-                                DispatchQueue.main.async {
-                                    selectHandler()
-                                    HapticManager.shared.success()
-                                }
-                            }
-                            
-                            isSheetShow = false
-                            
-                            priceText = ""
-                            aboutText = ""
-                            selectedTag = Tag.other
-                            spendingBool = false
-                        } else {
-                            HapticManager.shared.error()
+                Form {
+                    Section {
+                        HStack {
+                            Text("field_price".localized)
+                            Spacer()
+                            TextField("hint_necessarily".localized, text: $priceText)
+                                .keyboardType(.decimalPad)
+                                .multilineTextAlignment(.trailing)
+                                .frame(width: UIScreen.main.bounds.width / 3)
                         }
-                    } label: {
-                        Text("btn_next".localized)
+                        HStack {
+                            Text("field_about".localized)
+                            Spacer()
+                            TextField("hint_necessarily".localized, text: $aboutText)
+                                .multilineTextAlignment(.trailing)
+                                .frame(width: UIScreen.main.bounds.width / 3)
+                        }
+                        if spendingBool {
+                            HStack {
+                                Text("Тэг".localized)
+                                Spacer()
+                                Picker("", selection: $selectedTag) {
+                                    ForEach(Tag.allCases, id: \.self) { tag in
+                                        Text(tag.rawValue.localized)
+                                    }
+                                }
+                                .pickerStyle(.menu)
+                            }
+                            .animation(.default)
+                            .transition(.slide)
+                        }
+                    } footer: {
+                        Text("hint_payment".localized)
+                    }
+                }
+                .navigationBarTitle("label_newpayment".localized, displayMode: .inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            isSheetShow = false
+                        } label: {
+                            Text("btn_cancel".localized)
+                        }
+                    }
+                    
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            if !priceText.isEmpty && !aboutText.isEmpty {
+                                let priceStr = priceText.replacingOccurrences(of: ",", with: ".")
+                                guard let fl = Float(spendingBool ? "-\(priceStr)" : priceStr) else { return }
+                                let about = aboutText
+                                let tag = selectedTag
+                                
+                                DispatchQueue.global(qos: .userInitiated).async {
+                                    CoreDataManager.shared.addPayment(price: fl, about: about, tag: tag)
+                                    
+                                    DispatchQueue.main.async {
+                                        selectHandler()
+                                        HapticManager.shared.success()
+                                    }
+                                }
+                                
+                                isSheetShow = false
+                                
+                                priceText = ""
+                                aboutText = ""
+                                selectedTag = Tag.other
+                                spendingBool = false
+                            } else {
+                                HapticManager.shared.error()
+                            }
+                        } label: {
+                            Text("btn_next".localized)
+                        }
                     }
                 }
             }
