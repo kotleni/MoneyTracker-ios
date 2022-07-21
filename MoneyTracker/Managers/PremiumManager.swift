43865cae138a77b0e554e6f4b3a341e06a8bf69e
kotleni <yavarenikya@gmail.com>
2022-07-21 11:04:29 +0300

    Premium manager updated for safety

--- MoneyTracker/Managers/PremiumManager.swift
+++ MoneyTracker/Managers/PremiumManager.swift
@@ -11,49 +11,57 @@ import StoreKit
 /// Premium subscribe manager
 class PremiumManager {
     static let shared = PremiumManager()
+    
     private let productId = "moneytracker.preimium1";
     
     /// Check is shop available
     func isAvailable() async -> Bool {
-        let products = try! await Product.products(for: [productId])
-        return !(products.isEmpty)
+        do {
+            let products = try await Product.products(for: [productId])
+            return !(products.isEmpty)
+        } catch let error { print(error.localizedDescription) }
+        
+        return false
     }
     
     /// Try subscribe to premium
-    func trySubscribe() async -> Product.PurchaseResult {
-        let products = try! await Product.products(for: [productId])
-        let result = try! await products[0].purchase()
-        return result
+    func trySubscribe() async -> Product.PurchaseResult? {
+        do {
+            let products = try await Product.products(for: [productId])
+            let result = try await products[0].purchase()
+            return result
+        } catch let error { print(error.localizedDescription) }
+        
+        return nil
     }
     
     /// Check is premium subscribed
     func isPremiumExist() async -> Bool {
-        let products = try! await Product.products(for: [productId])
+        do {
+            let products = try await Product.products(for: [productId])
+            let state = await products[0].currentEntitlement
+            switch state {
+            case .verified( _):
+                print(".verifed")
+                return true
+            case .unverified( _, _):
+                print(".unverified")
+                return true
+            default:
+                return false
+            }
+        } catch let error { print(error.localizedDescription) }
         
-        if products.isEmpty {
-            return false
-        }
-        
-        let state = await products[0].currentEntitlement
-        
-        switch state {
-        case .verified( _):
-            print(".verifed")
-            return true
-        case .unverified( _, _):
-            print(".unverified")
-            return true
-        default:
-            return false
-        }
+        return false
     }
     
     /// Get premium price
     func getPremiumPrice() async -> String {
-        let products = try! await Product.products(for: [productId])
-        if products.isEmpty {
-            return ""
-        }
-        return products.first!.displayPrice
+        do {
+            let products = try await Product.products(for: [productId])
+            return products.first!.displayPrice
+        } catch let error { print(error.localizedDescription) }
+        
+        return ""
     }
 }
