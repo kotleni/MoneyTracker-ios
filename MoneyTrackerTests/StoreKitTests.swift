bac3b6fd9361f9b18b1b9d981c14e806e6ad8719
kotleni <yavarenikya@gmail.com>
2022-08-06 18:45:49 +0300

    Refactor

--- MoneyTrackerTests/StoreKitTests.swift
+++ MoneyTrackerTests/StoreKitTests.swift
@@ -64,12 +64,4 @@ class StoreKitTests: XCTestCase {
             XCTAssertTrue(result)
         }
     }
-    
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
