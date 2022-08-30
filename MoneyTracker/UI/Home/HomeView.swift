c0689590efa424807a111b5f6d3793701acf1e94
kotleni <yavarenikya@gmail.com>
2022-08-30 22:42:32 +0300

    Pie chart removed

--- MoneyTracker/UI/Home/HomeView.swift
+++ MoneyTracker/UI/Home/HomeView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import PieChart
 
 // MARK: fixme
 var isFirstHomeOpen = true
@@ -37,14 +36,6 @@ struct HomeView: View {
                             totalIncome: viewModel.balance.income,
                             totalOutcome: viewModel.balance.outcome,
                             priceType: viewModel.priceType)
-                        
-                        if (viewModel.balance.income + viewModel.balance.outcome) != 0 {
-                            PieChart(radius: 36, items: [
-                                ChartItem(name: "label_expenses".localized, value: abs(Double(viewModel.balance.outcome)), color: Color("AccentColor")),
-                                ChartItem(name: "label_income".localized, value: Double(viewModel.balance.income), color: Color(red: 45/255, green: 192/255, blue: 79/255))
-                            ])
-                            .frame(height: 110)
-                        }
                     } header: {
                         Text("label_statistic".localized)
                     }
